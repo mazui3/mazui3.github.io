f78eac44686fd8fc19bae1b34c89de008c01cac9
mazuisan <44691452+mazuisan@users.noreply.github.com>
2019-10-14 09:23:56 -0400

    position changed

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // let b1 = document.getElementById("B1");
 
-document.getElementById("img2").style.display = "none";
+
 
 document.getElementById("main-header").onclick = function () {
    document.getElementById("img2").style.display="block";
