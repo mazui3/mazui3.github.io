c7c96644ea68c999060d7a7acd8c7322a315d8c5
mazuisan <44691452+mazuisan@users.noreply.github.com>
2019-10-13 21:48:31 -0400

    add icon

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// let b1 = document.getElementById("B1");
+
+document.getElementById("img2").style.display = "none";
+
+document.getElementById("main-header").onclick = function () {
+   document.getElementById("img2").style.display="block";
+}
